96d94700ce5c41e4eca25c4e54ae960b3e83bbf0
cdeng1111 <jenniferdeng465@gmail.com>
2022-01-14 08:25:47 -0800

    ETL process done

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
+const pool = require('./db/index.js');
 
 const app = express();
 const port = 3000;
 
 //middleware
 app.use(express.json());
-
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
