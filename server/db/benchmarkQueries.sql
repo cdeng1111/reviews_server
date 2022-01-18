0165a36fd01fa7c5e5d97cecf1b3f472cc68e867
cdeng1111 <jenniferdeng465@gmail.com>
2022-01-17 21:27:17 -0800

    updated benchmark query analysis

--- server/db/benchmarkQueries.sql
+++ server/db/benchmarkQueries.sql
@@ -1,11 +1,12 @@
 -- psql -d reviewsdb -f ./db/benchmarkQueries.sql
-\timing
-select r.*, p.id as photo_id, p.url from reviews r
-left join photos p on r.id = p.review_id
-where r.product_id = 63609;
--- Time: 739.447 ms
 
-SELECT
+EXPLAIN ANALYZE select r.*, p.id as photo_id, p.url from reviews r
+left join photos p on r.id = p.review_id
+where r.product_id =
+	(SELECT floor(random() * 1000010 + 1)::int);
+--  Execution Time: 1017.116 ms (using EXPLAIN ANALYZE)
+
+EXPLAIN ANALYZE SELECT
          r.id,
          r.rating,
          r.summary,
@@ -23,20 +24,24 @@ SELECT
                 AS Photos FROM photos WHERE r.id = review_id
           )
       FROM reviews r
-      WHERE r.product_id = 63609 OFFSET 1 LIMIT 5
--- Time: 3109.744 ms
+      WHERE r.product_id = (SELECT floor(random() * 1000010 + 1)::int)
+       OFFSET 1 LIMIT 5
+--  Execution Time: 2204.477 ms (using EXPLAIN ANALYZE)
 
- SELECT json_build_object(
+
+
+EXPLAIN ANALYZE SELECT json_build_object(
       'product_id', product_id,
        'ratings',
            (SELECT json_object_agg(rating,num_reviews)
               FROM (SELECT rating, count(*) as num_reviews from reviews
-                  WHERE product_id = 63609 GROUP BY rating) r),
-
+                  WHERE product_id = (SELECT floor(random() * 1000010 + 1)::int)
+                  GROUP BY rating) r),
        'recommended',
             (SELECT json_object_agg(recommend,num_reviews)
             FROM (SELECT recommend, count(*) as num_reviews FROM reviews
-             WHERE product_id = 63609 group by recommend) re),
+             WHERE product_id = (SELECT floor(random() * 1000010 + 1)::int)
+             group by recommend) re),
     'characteristics',
              (SELECT json_object_agg
              ( name, json_build_object(
@@ -48,11 +53,11 @@ SELECT
               FROM characteristics c
               LEFT JOIN characteristic_reviews cr
               ON c.id = cr.characteristic_id
-              WHERE c.product_id = 63609
+              WHERE c.product_id = (SELECT floor(random() * 1000010 + 1)::int)
               GROUP BY  c.name, c.id
                 ) r
             )
      )
     FROM reviews
-    WHERE product_id = 63609;
-
+    WHERE product_id = (SELECT floor(random() * 1000010 + 1)::int);
+--  Execution Time: 5297.290 ms (using EXPLAIN ANALYZE)
