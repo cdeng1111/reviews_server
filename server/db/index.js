11a2383944adcb8c0c201981ebce58ae377acf0e
cdeng1111 <jenniferdeng465@gmail.com>
2022-01-21 11:11:16 -0800

    changed db params

--- server/db/index.js
+++ server/db/index.js
@@ -2,12 +2,20 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool(
-{
+  {
     user: process.env.DB_USER,
-    password: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DATABASE,
   });
+
+pool.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+})
 
 module.exports = pool;
