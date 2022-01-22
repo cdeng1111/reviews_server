982a31044692512db2865fc7697338fbd5d4cfcf
cdeng1111 <jenniferdeng465@gmail.com>
2022-01-22 14:47:37 -0800

    updated routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const router = require('./routes');
+// const router = require('./routes');
 // const newrelic = require('newrelic');
+const controller = require('./controller');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use('/', router);
+// app.use('/', router);
+
+app.get('/reviews', controller.getReviews);
+app.get('/reviews/meta', controller.getMeta);
+app.post('/reviews', controller.postReview);
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
